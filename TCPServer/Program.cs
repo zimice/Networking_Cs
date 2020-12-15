5cf1178d56e9156737116af7b9691b720ba399b0
zimice <simon.kochanek@me.com>
2020-12-15 06:53:24 +0100

    First Commit

---
+++ TCPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpServer server = new TcpServer(65525);
+        }
+    }
+}
